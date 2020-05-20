1814f8cfeba3a5986ff50014398c8f3afe5e8ef0
Antoine Cronier <antoine.cronier@tactfactory.com>
2020-05-20 16:30:43 +0200

    etape 3-1

--- src/main/java/com/tactfactory/mongocrawler/managers/MongoUserManager.java
+++ src/main/java/com/tactfactory/mongocrawler/managers/MongoUserManager.java
@@ -23,19 +23,21 @@ public class MongoUserManager {
   }
 
   public void run() {
-    String collection = this.selectCollection();
-    Integer option = this.selectionCollectionOption();
+    while (true) {
+      String collection = this.selectCollection();
+      Integer option = this.selectionCollectionOption();
 
-    switch (option) {
-    case 1:
-        this.findIncollection(collection);
-      break;
-    case 2:
+      switch (option) {
+      case 1:
+        FindIterable<Document> datas = this.findIncollection(collection);
+        break;
+      case 2:
         this.insertInCollection(collection);
-      break;
+        break;
 
-    default:
-      break;
+      default:
+        break;
+      }
     }
   }
 
@@ -45,7 +47,9 @@ public class MongoUserManager {
   }
 
   private int loop;
-  private void findIncollection(String collection) {
+
+  private FindIterable<Document> findIncollection(String collection) {
+    FindIterable<Document> result = null;
     final FindIterable<Document> documents = this.db.getCollection(collection).find();
 
     final Map<Integer, String> fields = new HashMap<>();
@@ -54,23 +58,41 @@ public class MongoUserManager {
 
     loop = 1;
 
-    documents.forEach(x -> x.keySet()
-        .forEach(y -> {
-          if(!fields.containsValue(y)) {
-            String typeString = x.get(y) == null ? "null" : x.get(y).getClass().getName();
-            if (!typeString.equals(ArrayList.class.getName())) {
-              fieldsType.put(y, typeString);
-              fields.put(loop,y);
-              fieldsChoice.add(String.format("%s : %s", loop, y));
-              MongoUserManager.this.loop++;
-            }else {
-              //System.out.println("Array");
-            }
-          }
-        }));
-    String choice = ScannerUtil.getInstance().selectStringFromIntChoice("Sélection un champ", fieldsChoice.toArray(),fields);
+    documents.forEach(x -> x.keySet().forEach(y -> {
+      if (!fields.containsValue(y)) {
+        String typeString = x.get(y) == null ? "null" : x.get(y).getClass().getName();
+        if (!typeString.equals(ArrayList.class.getName())) {
+          fieldsType.put(y, typeString);
+          fields.put(loop, y);
+          fieldsChoice.add(String.format("%s : %s", loop, y));
+          MongoUserManager.this.loop++;
+        } else {
+          // System.out.println("Array");
+        }
+      }
+    }));
+    String choice = ScannerUtil.getInstance().selectStringFromIntChoice("Sélection un champ", fieldsChoice.toArray(),
+        fields);
 
-    System.out.println(String.format("Insérer une valeur pour %s", choice));
+    final List<String> operators = new ArrayList<>();
+    operators.add("=");
+    operators.add(">");
+    operators.add("<");
+    operators.add(">=");
+    operators.add("<=");
+    operators.add("!=");
+    final List<String> operatorChoice = new ArrayList<>();
+    operatorChoice.add(String.format("0 - %s", operators.get(0)));
+    operatorChoice.add(String.format("1 - %s", operators.get(1)));
+    operatorChoice.add(String.format("2 - %s", operators.get(2)));
+    operatorChoice.add(String.format("3 - %s", operators.get(3)));
+    operatorChoice.add(String.format("4 - %s", operators.get(4)));
+    operatorChoice.add(String.format("5 - %s", operators.get(5)));
+
+    int choiceOperator = ScannerUtil.getInstance().selectInt("Sélection un opérateur", operatorChoice.toArray(), 0,
+        operatorChoice.size() - 1);
+
+    System.out.println(String.format("Insérer une valeur pour %s avec l'opérateur %s", choice, operators.get(choiceOperator)));
 
     Class<?> cls = null;
     try {
@@ -82,18 +104,61 @@ public class MongoUserManager {
 
     Bson filter = null;
     if (cls == String.class) {
-      filter = Filters.eq(choice, ScannerUtil.getInstance().inputString());
-    }else if(cls == Integer.class) {
-      filter = Filters.eq(choice, ScannerUtil.getInstance().inputInt());
+      switch (choiceOperator) {
+      case 0:
+        filter = Filters.eq(choice, ScannerUtil.getInstance().inputString());
+        break;
+      case 5:
+        filter = Filters.ne(choice, ScannerUtil.getInstance().inputString());
+        break;
+
+      default:
+        System.out
+            .println(String.format("Opération non autorisé pour l'opérateur %s", operatorChoice.get(choiceOperator)));
+        break;
+      }
+
+    } else if (cls == Integer.class) {
+      switch (choiceOperator) {
+      case 0:
+        filter = Filters.eq(choice, ScannerUtil.getInstance().inputInt());
+        break;
+
+      case 1:
+        filter = Filters.gt(choice, ScannerUtil.getInstance().inputInt());
+        break;
+
+      case 2:
+        filter = Filters.lt(choice, ScannerUtil.getInstance().inputInt());
+        break;
+
+      case 3:
+        filter = Filters.gte(choice, ScannerUtil.getInstance().inputInt());
+        break;
+
+      case 4:
+        filter = Filters.lte(choice, ScannerUtil.getInstance().inputInt());
+        break;
+
+      case 5:
+        filter = Filters.ne(choice, ScannerUtil.getInstance().inputInt().intValue());
+        break;
+
+      default:
+        System.out
+            .println(String.format("Opération non autorisé pour l'opérateur %s", operatorChoice.get(choiceOperator)));
+        break;
+      }
     }
 
-    FindIterable<Document> finded = this.db.getCollection(collection).find(filter);
+    result = this.db.getCollection(collection).find(filter);
 
-    for (Document document : finded) {
+    for (Document document : result) {
       System.out.println(document.toJson());
     }
 
     System.out.println("find");
+    return result;
   }
 
   private Integer selectionCollectionOption() {
