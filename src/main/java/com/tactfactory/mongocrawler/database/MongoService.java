ecf8e679d275b75a818bf2ba2708a73633877e9b
Antoine Cronier <antoine.cronier@tactfactory.com>
2020-05-20 10:00:25 +0200

    Etape 1

---
+++ src/main/java/com/tactfactory/mongocrawler/database/MongoService.java
@@ -0,0 +1,33 @@
+package com.tactfactory.mongocrawler.database;
+
+import java.util.Arrays;
+
+import com.mongodb.MongoClientSettings;
+import com.mongodb.ServerAddress;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+
+public class MongoService {
+
+  private final MongoClient mongoClient;
+
+  public MongoService() {
+    this("127.0.0.1", 27017);
+  }
+
+  public MongoService(final Integer port) {
+    this("127.0.0.1", port);
+  }
+
+  public MongoService(final String host) {
+    this(host, 27017);
+  }
+
+  public MongoService(final String host, final Integer port) {
+    mongoClient = MongoClients.create(
+        MongoClientSettings.builder()
+                .applyToClusterSettings(builder ->
+                        builder.hosts(Arrays.asList(new ServerAddress(host, port))))
+                .build());
+  }
+}
